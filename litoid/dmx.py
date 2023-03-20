8fd5b5302628977d837f898adcd0dfe61e9950a5
Tom Ritchford <tom@swirly.com>
2023-03-20 16:00:08 +0100

    Add litoid/dmx.py

---
+++ litoid/dmx.py
@@ -0,0 +1,30 @@
+from . thread_queue import ThreadQueue
+from functools import cached_property
+from queue import Queue
+from threading import Thread
+import datacls
+import pyenttec
+
+
+@datacls
+class DMX(ThreadQueue):
+    port: str
+
+    @cached_property
+    def connection(self):
+        return pyenttec.DMXConnection(self.port)
+
+    @cached_property
+    def frame(self):
+        return self.connection.dmx_frame
+
+    def put(self, function, *args):
+        assert callable(function)
+        super().put(function, *args)
+
+    def render(self):
+        self.connection.render()
+
+    def callback(self, f, *args):
+        f(self.frame, *args)
+        self.render()
