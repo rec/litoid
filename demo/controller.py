6a4b79472a46eaff3b6921bb1bd235993a3d8e68
Tom Ritchford <tom@swirly.com>
2023-03-20 14:33:43 +0100

    Tweak demo

--- demo/controller.py
+++ demo/controller.py
@@ -2,6 +2,10 @@ import time
 import pyenttec as dmx
 
 con = dmx.DMXConnection('/dev/cu.usbserial-6AYL2V8Z')
+con.dmx_frame[0] = con.dmx_frame[1] = con.dmx_frame[2] = 1 * 100
+con.render()
+time.sleep(3)
+
 con.dmx_frame[0] = con.dmx_frame[1] = con.dmx_frame[2] = 0 * 100
 con.render()
-time.sleep(0.01)
+time.sleep(0.1)
