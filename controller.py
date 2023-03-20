4182ae453187bd89b71e8c6970ead1a8b28c4524
Tom Ritchford <tom@swirly.com>
2023-03-20 13:15:58 +0100

    Remove experimental code

--- controller.py
+++ controller.py
@@ -1,50 +1,7 @@
-from functools import cached_property
-from pyftdi.ftdi import Ftdi
-import numpy as np
+import time
+import pyenttec as dmx
 
-FTDI_VENDOR_ID = 0x0403
-FTDI_PRODUCT_ID = 0x6001
-FTDI_URL = 'ftdi:///1'
-FTDI_BAUDRATE = 250_000
-FTDI_LINE_PROPERTIES = dict(bits=8, stopbit=2, parity='N', break_=False)
-
-
-class OpenDmxController:
-    @cached_property
-    def ftdi(self):
-        f = Ftdi()
-        f.open_from_url(FTDI_URL)
-        f.reset()
-        f.set_baudrate(FTDI_BAUDRATE)
-        f.set_line_property(**FTDI_LINE_PROPERTIES)
-        return f
-
-    @cached_property
-    def _frame(self):
-        return bytearray(513)
-
-    @cached_property
-    def levels(self):
-        frame = np.array(self._frame, dtype='uint8', copy=False)
-        return frame[1:]
-
-    def transmit(self):
-        # Write
-        self.ftdi.set_break(True)
-        self.ftdi.set_break(False)
-        self.ftdi.write_data(self._frame)
-
-    def close(self):
-        self.ftdi.close()
-
-
-def main():
-    od = OpenDmxController()
-    od.levels += 255
-
-    while True:
-        od.transmit()
-        import time
-        time.sleep(0.1)
-
-main()
+con = dmx.DMXConnection('/dev/cu.usbserial-6AYL2V8Z')
+con.dmx_frame[0] = con.dmx_frame[1] = con.dmx_frame[2] = 0 * 100
+con.render()
+time.sleep(0.01)
