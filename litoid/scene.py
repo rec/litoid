5460817ed7ffc668aa899ded7a032133b8c4845d
Tom Ritchford <tom@swirly.com>
2023-03-20 19:50:20 +0100

    Add more to scene.py

--- litoid/scene.py
+++ litoid/scene.py
@@ -2,14 +2,14 @@ from . dmx import DMX
 from . midi import MidiInput
 from . timed_heap import TimedHeap
 from functools import cached_property
+from threading import Lock
 import datacls
 
 
 @datacls
-class Universe:
-    dmx_port: str
+class State:
+    dmx_port: str = '/dev/cu.usbserial-6AYL2V8Z'
     midi_input_name: str | None = None
-    timed_heap: TimedHeap = datacls.field(TimedHeap)
 
     @cached_property
     def dmx(self):
@@ -17,9 +17,60 @@ class Universe:
 
     @cached_property
     def midi_input(self):
-        return MidiInput(self.midi_input_name)
+        return MidiInput(self.scene.midi_callback, self.midi_input_name)
+
+    @cached_property
+    def timed_heap(self):
+        return TimedHeap()
+
+    @cached_property
+    def scene(self):
+        return SceneHolder(self)
+
+
+class Scene:
+    def load(self, state: State):
+        pass
+
+    def midi_callback(self, state: State, msg):
+        pass
+
+    def unload(self, state: State):
+        pass
 
 
 @datacls
-class Scene:
-    pass
+class Compose(Scene):
+    scenes: list[Scene] = datacls.field(list)
+    reverse_midi: bool = False
+
+    def load(self, state: State):
+        return any(s.load(state) for s in self.scenes)
+
+    def midi_callback(self, state: State, msg):
+        scenes = reversed(self.scenes) if self.reverse_midi else self.scenes
+        return any(s.midi_callback(state, msg) for s in scenes)
+
+    def unload(self, state: State):
+        return any(s.unload(state) for s in reversed(self.scenes))
+
+
+@datacls.mutable
+class SceneHolder:
+    state: State
+    _scene: Scene = datacls.field(Scene)
+    _lock: Lock = datacls.field(Lock)
+
+    @property
+    def scene(self) -> Scene:
+        return self._scene
+
+    @scene.setter
+    def set_scene(self, scene: Scene):
+        with self._lock:
+            self._scene.unload(self.state)
+            self._scene = scene
+            self._scene.load(self.state)
+
+    def midi_callback(self, msg):
+        self._scene.midi_callback(self.state, msg)
