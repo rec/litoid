877843465075be19442a7c6881960c538fc3ca32
Tom Ritchford <tom@swirly.com>
2023-03-22 14:25:26 +0100

    Add numpy to dependencies

--- demo/controller.py
+++ demo/controller.py
@@ -1,7 +1,7 @@
 import time
 import pyenttec as dmx
 
-con = dmx.DMXConnection('/dev/cu.usbserial-6AYL2V8Z')
+con = dmx.DMXConnection('/dev/cu.usbserial-6AYL2V8Z', use_numpy=True)
 con.dmx_frame[0] = con.dmx_frame[1] = con.dmx_frame[2] = 1 * 100
 con.render()
 time.sleep(3)
