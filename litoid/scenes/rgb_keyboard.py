6cdb9cce8f73b4748258f5fcbdef36ebf8930539
Tom Ritchford <tom@swirly.com>
2023-03-24 19:48:00 +0100

    New Keyboard tracer

--- litoid/scenes/rgb_keyboard.py
+++ litoid/scenes/rgb_keyboard.py
@@ -1,22 +1,26 @@
 from pynput.keyboard import Events
 from ..state.scene import Scene
 
-KEYS = 'rgbefv'
 
+class RGBKeyboardOld(Scene):
+    KEYS = 'rgbefv'
 
-class RGBKeyboard(Scene):
     def callback(self, state, msg):
         if not isinstance(msg, Events.Press):
             return
-
         try:
-            i = KEYS.index(getattr(msg.key, 'char', None))
+            i = self.KEYS.index(getattr(msg.key, 'char', None))
         except Exception:
             return
-        inc, li = divmod(i, 3)
+        inc, li = divmod(i, (len(self.KEYS) // 2))
         inc = 1 - (2 * inc)
         state.dmx.frame[li] = max(0, min(255, state.dmx.frame[li] + inc))
-        print(KEYS[li], *state.dmx.frame[0:3])
+        state.dmx.render()
+        print(self.KEYS[li], *state.dmx.frame[0:len(self.KEYS)])
+
+
+class RGBKeyboard(RGBKeyboardOld):
+    KEYS = 'qwertyuasdfghj'
 
 
 if __name__ == '__main__':
