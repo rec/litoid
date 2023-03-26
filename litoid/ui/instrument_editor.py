b00a9b46d88ab84e21ee6244ad1f810785852a3e
Tom Ritchford <tom@swirly.com>
2023-03-26 21:11:31 +0200

    Delete parts of the editor

--- litoid/ui/instrument_editor.py
+++ litoid/ui/instrument_editor.py
@@ -8,18 +8,6 @@ class InstrumentEditorApp(App):
         # ("d", "toggle_dark", "Toggle dark mode")
     ]
 
-    def run(self, *a, **ka):
-        import threading
-        import time
-
-        def light():
-            # Hacky fix for dark mode
-            time.sleep(0.001)
-            self.call_from_thread(setattr, self, 'dark', False)
-
-        threading.Thread(target=light).start()
-        super().run(*a, **ka)
-
     def compose(self):
         yield Header()
         yield Footer()
