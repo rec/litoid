e703a19b903211969f5613e7a8b0a22d1bcf1438
Tom Ritchford <tom@swirly.com>
2023-03-27 13:40:41 +0200

    Tweak demo/simple.py

--- demo/simple.py
+++ demo/simple.py
@@ -1,6 +1,8 @@
 import PySimpleGUI as sg
 
 sg.theme('DarkAmber')   # Add a touch of color
+sg.set_options(font=('Helvetica', 18))
+
 # All the stuff inside your window.
 layout = [  [sg.Text('Some text on Row 1')],
             [sg.Text('Enter something on Row 2'), sg.InputText()],
