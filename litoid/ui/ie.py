443eedd9bfdbc8efefb8919f0e8f2f75ed771f68
Tom Ritchford <tom@swirly.com>
2023-03-31 15:23:34 +0200

    Remove experimental callback system

--- litoid/ui/ie.py
+++ litoid/ui/ie.py
@@ -1,7 +1,6 @@
 from . import lamp_page, ui
 from ..io import midi
 from ..state import scene, state as _state
-from functools import cached_property
 import PySimpleGUI as sg
 import datacls
 
@@ -21,8 +20,6 @@ class InstrumentEditorApp(ui.UI):
         return self.lamp.instrument
 
     def callback(self, msg):
-        if not True:
-            return Callback(ie=self, **msg.asdict())()
         if msg.key == 'tabgroup':
             name = msg.values['tabgroup'].split('.')[0]
             self.lamp = self.lamps[name]
@@ -112,71 +109,6 @@ class InstrumentEditorApp(ui.UI):
     def reset_levels(self):
         for i in range(len(self.lamp.instrument.channels)):
             self.reset_level(i)
-
-
-@datacls
-class Callback(ui.Message):
-    ie: InstrumentEditorApp
-
-    def __call__(self):
-        getattr(self, '_' + self.command, self._error)()
-
-    def __getattr__(self, k):
-        if k.startswith('_'):
-            raise AttributeError(k)
-        return getattr(self.ie, k)
-
-    @cached_property
-    def channel(self):
-        lamp_name, channel, comp = self.key.split('.')
-        assert lamp_name == self.lamp.name
-        assert self.comp == comp
-        return channel
-
-    @cached_property
-    def command(self):
-        return self.key.partition('.')[-1]
-
-    @cached_property
-    def value(self):
-        return self.values.get(self.key)
-
-    def _error(self):
-        print('Do not understand message', self.msg.key)
-
-    def _tabgroup(self):
-        name = self.values['tabgroup'].split('.')[0]
-        self.ie.lamp = self.lamps[name]
-
-    def _blackout(self):
-        self.lamp.blackout()
-        self.reset_levels()
-
-    def _slider(self):
-        self.set_input(int(self.value))
-
-    def _combo(self):
-        self.set_input(self.instrument.full_value_names[self.value])
-
-    def _input(self):
-        try:
-            level = max(0, min(255, int(self.value)))
-        except (ValueError, TypeError):
-            level = 0
-
-        self.set_input(level)
-        if name := self.instrument.level_to_name(self.channel, level):
-            self.set_value('combo', name)
-        else:
-            self.set_value('slider', level)
-
-    def set_input(self, value):
-        self.set_value('input', value)
-        self.lamp[self.channel] = value
-
-    def set_value(self, key, value):
-        k = f'{self.lamp.name}.{self.channnel}.{key}'
-        self.window[k].update(value=value)
 
 
 class MidiScene(scene.Scene):
