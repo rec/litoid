f2e767fd4a52d16db729ea5190a1c8a1cf14ab69
Tom Ritchford <tom@swirly.com>
2023-03-31 12:44:54 +0200

    Tweak demo/closer.py with new info

--- demo/closer.py
+++ demo/closer.py
@@ -3,6 +3,10 @@ import PySimpleGUI as sg
 layout = [[sg.Text('Command-Q problem')], [sg.Button('Exit')]]
 window = sg.Window('Title', layout, enable_close_attempted_event=True)
 
+# https://stackoverflow.com/a/70913395
+window.finalize()
+window.TKroot.bind_all("<Command-q>", print)
+
 try:
     while True:
         event, values = window.read()
