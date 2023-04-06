f36aa39af5f933e60ae6f93941df3e36582dd882
Tom Ritchford <tom@swirly.com>
2023-04-06 15:20:37 +0200

    Modify demo/tabgroup.py

--- demo/tabgroup.py
+++ demo/tabgroup.py
@@ -3,10 +3,14 @@ import PySimpleGUI as sg
 sg.theme('DarkAmber')   # Add a touch of color
 sg.set_options(font=('Helvetica', 18))
 
+
+
+
 # All the stuff inside your window.
 page1 = [
-    [sg.Text('Page 1')],
+    [sg.Text('Page 1', k='text-1')],
 ]
+tab1 = sg.Tab('tab1', page1, k='tab-1')
 
 # All the stuff inside your window.
 page2 = [
@@ -14,20 +18,22 @@ page2 = [
     [sg.Button('Ok'), sg.Button('Cancel')]
 ]
 
-tab1 = sg.Tab('tab1', page1, k='+TAB1+')
-tab2 = sg.Tab('tab', page2, k='+TAB2+')
+tab2 = sg.Tab('tab', page2, k='tab-2')
 
-tabgroup = sg.TabGroup([[tab1, tab2]], enable_events=True)
+tabgroup = sg.TabGroup([[tab1, tab2]], enable_events=True, k='tabgroup')
 layout = [[tabgroup]]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Window Title', layout, finalize=True)
+window.bind('<FocusIn>', '<FocusIn>')
+window.bind('<FocusOut>', '<FocusOut>')
+
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # ifr clicks cancel
         break
-    print('You entered ', values[0])
+    print('You entered ', event, values)
 
 window.close()
