d9fd89718847082ba423c7cdc735a866a98d4879
Tom Ritchford <tom@swirly.com>
2023-04-06 15:20:37 +0200

    Modify demo/keys.py

--- demo/keys.py
+++ demo/keys.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from functools import partial
 
 layout = [
     [sg.Button("ok", size=(10, 2), key='button1'),
@@ -6,9 +7,27 @@ layout = [
 ]
 window = sg.Window('Hotkeys', layout, use_default_focus=False, finalize=True)
 button1, button2 = window['button1'], window['button2']
+USE_TK = not True
 
-window.bind("<Alt_L><o>", "ALT-o")
-window.bind("<Alt_L><x>", "ALT-x")
+window.TKroot
+
+keys = '<alt>+<f>', '<ctrl>+<f>', '<cmd>+<f>', '<Left>', '<Right>'
+keys = '<Alt_L>+<f>', '<Control_L>+<f>', '<Left>', '<Right>'
+keys = '<Command-f>', '<Control-f>', '<Shift-f>'
+
+if not False:
+    for k in keys:
+        if USE_TK:
+            window.TKroot.bind(k, partial(print, k))
+        else:
+            window.bind(k, k)
+else:
+    def key_cb(event):
+        print(str(event))
+        print(event.char, event.state, type(event.state))
+
+
+    window.TKroot.bind('<Key>', key_cb)
 
 button1.Widget.configure(underline=0, takefocus=0)
 button2.Widget.configure(underline=1, takefocus=0)
