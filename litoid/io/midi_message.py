b6431bd50d15e6c15e77d4040692fa9341228236
Tom Ritchford <tom@swirly.com>
2023-04-11 17:34:13 +0200

    Improve MidiMessage special cases

--- litoid/io/midi_message.py
+++ litoid/io/midi_message.py
@@ -1,7 +1,8 @@
 import clsprop
 import xmod
 
-_SYSEX_STATUS = 0xF0
+_PITCH = 0xe0
+_SYSEX = 0xF0
 
 
 @xmod
@@ -12,14 +13,20 @@ class MidiMessage:
     __slots__ = 'data', 'time'
 
     def __init__(self, data=None, time=0, **kwargs):
-        if data is None:
-            data = [kwargs.pop(f, 0) for f in self.fields]
-            if kwargs:
-                s = 's' * (len(kwargs) != 1)
-                raise ValueError(f'Unknown parameter{s}: {sorted(kwargs)}')
-
-        self.data = data
         self.time = time
+
+        if data is not None:
+            self.data = data
+        else:
+            self.data = (
+                [self.status_start]
+                + [kwargs.pop(f, 0) or 0 for f in self.fields]
+            )
+
+        if bad := [k for k, v in kwargs.items() if v is not None]:
+            assert (data is None) or not kwargs, f'{data=} {kwargs=}'
+            s = 's' * (len(bad) != 1)
+            raise ValueError(f'Extra parameter{s}: {list(kwargs)}')
 
     def __len__(self):
         return len(self.data)
@@ -33,7 +40,11 @@ class MidiMessage:
 
     @clsprop
     def size(cls) -> int:
-        return len(cls.fields) + (not cls.has_channel)
+        return (
+            len(cls.fields)
+            + (not cls.has_channel)
+            + (cls.status_start == _PITCH)
+        )
 
     @property
     def status(self):
@@ -54,7 +65,7 @@ class MidiMessage:
         if cls is None:
             raise ValueError(f'{data=} is not a MIDI packet')
 
-        if cls.status_start != _SYSEX_STATUS and len(data) != cls.size:
+        if cls.status_start != _SYSEX and len(data) != cls.size:
             msg = f'Wrong MIDI packet length: {len(data)} != {cls.size}'
             raise ValueError(msg)
 
@@ -64,9 +75,16 @@ class MidiMessage:
 
 
 class MidiChannelMessage(MidiMessage):
-    def __init__(self, data=None, time=0, *, channel=0, **kwargs):
-        status = channel + self.status_start
-        super().__init__(data, time, status=status, **kwargs)
+    def __init__(self, data=None, time=0, **kwargs):
+        channel = kwargs.pop('channel', None)
+
+        if is_pitch := self.status_start == _PITCH:
+            pitch = kwargs.pop('pitch', 0)
+        super().__init__(data, time, **kwargs)
+        if is_pitch:
+            self.pitch = pitch
+        if channel is not None:
+            self.channel = channel
 
     @property
     def channel(self):
@@ -81,12 +99,15 @@ class MidiChannelMessage(MidiMessage):
 
 
 def _has_channel(status: int) -> bool:
-    return status < _SYSEX_STATUS
+    return status < _SYSEX
 
 
 def _class(status_start, name, *props):
     if _has_channel(status_start):
         parent = MidiChannelMessage
+        if status_start == _PITCH:
+            fields = 'pitch',
+
         fields = 'channel', *props
     else:
         parent = MidiMessage
@@ -100,7 +121,7 @@ def _class(status_start, name, *props):
     }
 
     props = {prop: _prop(i + 1, prop) for i, prop in enumerate(props)}
-    cls = type(name, (parent,), members | props)
+    cls = type(name, (parent,), members | _pitch(status_start) | props)
     globals()[name] = cls
     return cls
 
@@ -118,7 +139,7 @@ def _prop(i, field):
 
 def _init(parent, fields):
     # Construct the constructor!
-    if params_in := ', '.join(f'{f}=0' for f in fields):
+    if params_in := ', '.join(f'{f}=None' for f in fields):
         params_in = f', *, {params_in}'
 
     if params_out := ', '.join(f'{f}={f}' for f in fields):
@@ -129,6 +150,24 @@ def _init(parent, fields):
     init = eval(f'{definition} {result}')
     init.__name__ = '__init__'
     return init
+
+
+def _pitch(status_start):
+    if status_start != _PITCH:
+        return {}
+
+    pitch_offset = 0x2000
+
+    def getter(self):
+        return 0x80 * self.pitch_hsb + self.pitch_lsb - self.pitch_offset
+
+    def setter(self, p):
+        self.pitch_hsb, self.pitch_lsb = divmod(p + self.pitch_offset, 0x80)
+
+    return {
+        'pitch': property(getter, setter),
+        'pitch_offset': pitch_offset,
+    }
 
 
 def _repr(fields):
@@ -148,29 +187,29 @@ _CHANNEL = (
     _class(0xb0, 'ControlChange', 'control', 'value'),
     _class(0xc0, 'ProgramChange', 'program'),
     _class(0xd0, 'Aftertouch', 'value'),
-    _class(0xe0, 'Pitchwheel', 'pitch'),
+    _class(_PITCH, 'Pitch', 'pitch_lsb', 'pitch_hsb'),
 )
 
 _SYSTEM = (
     # System common messages.
-    _class(0xf0, 'Sysex'),
+    _class(_SYSEX, 'Sysex'),
     _class(0xf1, 'QuarterFrame', 'frame_type', 'frame_value'),
     _class(0xf2, 'Songpos', 'pos'),
     _class(0xf3, 'SongSelect', 'song'),
 
-    None,  # 0xf4 is undefined
-    None,  # 0xf5 is undefined
+    None,  # 0xf4
+    None,  # 0xf5
     _class(0xf6, 'TuneRequest'),
-    None,  # 0xf7 is undefined
+    None,  # 0xf7 is sysex end
 
     # System real time messages.
     _class(0xf8, 'Clock'),
-    None,  # 0xf9 is undefined
+    None,  # 0xf9
     _class(0xfa, 'Start'),
     _class(0xfb, 'Continue'),
 
     _class(0xfc, 'Stop'),
-    None,  # 0xfd is undefined
+    None,  # 0xfd
     _class(0xfe, 'ActiveSensing'),
     _class(0xff, 'Reset'),
 )
