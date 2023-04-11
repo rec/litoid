23ef4f645c8d6b1ee3ac76efbb1d1a9134553bb5
Tom Ritchford <tom@swirly.com>
2023-04-11 20:31:06 +0200

    Serialize MidiRecorder

--- litoid/io/midi_recorder.py
+++ litoid/io/midi_recorder.py
@@ -2,9 +2,11 @@ from . import midi_message
 from functools import cached_property
 import datacls
 import numpy as np
+import time
 
 INITIAL_SIZE = 16
 RESIZE_RATIO = 2
+SEP = '-'
 
 
 @datacls.mutable
@@ -31,15 +33,62 @@ class MidiTrack:
         c = self.byte_width * self.count
         self.data[c:c + self.byte_width] = data
 
+    def asdict(self):
+        return {
+            'data': self.data[0:self.byte_width * self.count],
+            'times': self.times[0:self.count],
+        }
+
+    @classmethod
+    def fromdict(cls, data, times):
+        byte_width = len(data) // len(times)
+        assert byte_width * len(data) == len(times)
+        self = cls(byte_width)
+        self.__data__.update(data=data, times=times)
+        return self
+
 
 class MidiRecorder:
-    def __init__(self):
-        self.tracks = {}
+    def __init__(self, tracks=None, first_time=None):
+        self.tracks = {} if tracks is None else tracks
+        self.first_time = first_time
+
+    def start(self):
+        self.first_time = time.time()
 
     def record(self, msg: midi_message.MidiMessage):
+        if self.first_time is None:
+            self.first_time = msg.time
+
         keysize = 2 if isinstance(msg, midi_message.ControlChange) else 1
         key, data = msg.data[:keysize], msg.data[keysize:]
         if not (track := self.tracks.get(key)):
             track = MidiTrack(len(data))
             self.tracks[key] = track
         track.append(msg.time, *data)
+
+    def asdict(self):
+        d = {}
+
+        for key, track in sorted(self.tracks.items()):
+            key = SEP.join(str(i) for i in key)
+            for k, v in track.asdict().items():
+                subkey = SEP.join((key, k))
+                d[subkey] = v
+
+        if self.first_time is not None:
+            d['first_time'] = np.array([self.first_time])
+        return d
+
+    @classmethod
+    def fromdict(cls, d):
+        result = {}
+        for subkey, v in d.items():
+            if subkey != 'first_time':
+                *key, k = subkey.split(SEP)
+                key = tuple(int(i) for i in key)
+                result.setdefault(key, {})[k] = v
+
+        tracks = {k: MidiTrack.fromdict(**v) for k, v in result.items()}
+
+        return cls(tracks, d.get('first_time'))
