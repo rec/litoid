095e0aba0b49e2383543d4e30a0da940bb6af5af
Tom Ritchford <tom@swirly.com>
2023-04-15 15:08:58 +0200

    Fix tracks offsets and add typing

--- litoid/io/midi/track.py
+++ litoid/io/midi/track.py
@@ -2,8 +2,8 @@ from functools import cached_property
 import datacls
 import numpy as np
 
-INITIAL_SIZE = 16
-RESIZE_RATIO = 2
+INITIAL_SIZE = 0x200
+RESIZE_RATIO = 1.5
 
 
 @datacls.mutable
@@ -13,39 +13,31 @@ class MidiTrack:
 
     @cached_property
     def times(self):
-        return np.empty(INITIAL_SIZE)
-
-    @property
-    def first_time(self) -> float | None:
-        return self.times[0] if len(self.times) else None
-
-    def __len__(self):
-        return self.count
-
-    def __getitem__(self, i):
-        b = i * self.byte_width
-        return self.times[i], self.data[b:b + self.byte_width]
-
-    def __setitem__(self, i, v):
-        time, data = v
-        self.times[i] = time
-        b = i * self.byte_width
-        self.data[b:b + self.byte_width] = data
+        return np.empty(INITIAL_SIZE, dtype='float64')
 
     @cached_property
     def data(self):
         return np.empty(INITIAL_SIZE * self.byte_width, dtype='uint8')
 
-    def append(self, time, *data):
+    @property
+    def first_time(self) -> float | None:
+        return self.times[0] if len(self.times) else None
+
+    def get_message(self, i) -> tuple[np.ndarray, float]:
+        b = i * self.byte_width
+        return self.data[b:b + self.byte_width]
+
+    def append(self, data: np.ndarray, time: float):
         assert self.byte_width == len(data)
 
         if self.count >= len(self.times):
-            self.times.resize(RESIZE_RATIO * len(self.times))
-            self.data.resize(RESIZE_RATIO * len(self.data))
+            _resize(self.data, self.times)
 
+        b = self.count * self.byte_width
+        self.data[b:b + self.byte_width] = data
         self.times[self.count] = time
-        c = self.byte_width * self.count
-        self.data[c:c + self.byte_width] = data
+
+        self.count += 1
 
     def asdict(self):
         return {
@@ -57,6 +49,12 @@ class MidiTrack:
     def fromdict(cls, data, times):
         byte_width = len(data) // len(times)
         assert byte_width * len(data) == len(times)
+
         self = cls(byte_width)
         self.__data__.update(data=data, times=times)
         return self
+
+
+def _resize(*arrays):
+    for a in arrays:
+        a.resize(round(len(a) * RESIZE_RATIO + 0.5))
