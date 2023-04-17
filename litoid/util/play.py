ea8507e505cac83028386b12ccce3a4b062933c1
Tom Ritchford <tom@swirly.com>
2023-04-17 11:58:15 +0200

    Prevent two sounds from playing at once

--- litoid/util/play.py
+++ litoid/util/play.py
@@ -1,14 +1,23 @@
-from threading import Thread
+from threading import Thread, Lock
 from pathlib import Path
 import xmod
 import simpleaudio as sa
 
 DEFAULT = Path(__file__).parents[2] / 'audio/Click Mini 3.wav'
+_RUNNING = False
+_LOCK = Lock()
 
 
 def play_now(filename=None):
+    global _RUNNING
+    with _LOCK:
+        if _RUNNING:
+            return
+        _RUNNING = True
+
     wave_obj = sa.WaveObject.from_wave_file(str(filename or DEFAULT))
     wave_obj.play().wait_done()
+    _RUNNING = False
 
 
 @xmod
