b55e78e9e44108db1f7d4de1815bba3eed76e05e
Tom Ritchford <tom@swirly.com>
2023-04-17 12:34:06 +0200

    Add an entry to recorder.report()

--- litoid/io/midi/recorder.py
+++ litoid/io/midi/recorder.py
@@ -30,7 +30,8 @@ class MidiRecorder:
 
     def report(self):
         return {
-            'event_count': sum(t.count for t in self.tracks.values()),
+            'event_count': {k: t.count for k, t in self.tracks.items()},
+            'total_event_count': sum(t.count for t in self.tracks.values()),
             'track_count': len(self.tracks),
         }
 
