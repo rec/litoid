36d89655a9991fb5bfc014d45d8a4f52c3702798
Tom Ritchford <tom@swirly.com>
2023-04-24 12:25:26 +0200

    Refactor canvas_window.py slightly

--- litoid/ui/canvas_window.py
+++ litoid/ui/canvas_window.py
@@ -4,25 +4,26 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
-def draw_figure(window, figure):
-    canvas = window['test.canvas'].TKCanvas
+def draw_figure(window, figure, canvas):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
     return figure_canvas_agg
 
 
-def make_figure(window):
+def make_figure(window, canvas_key):
     dataSize = 1000
     xData = np.random.randint(100, size=dataSize)
     yData = np.linspace(0, dataSize, num=dataSize, dtype=int)
 
     figure = plt.figure()
+
     plt.plot(xData, yData, '.k')
-    draw_figure(window, figure)
+    canvas = window[canvas_key].TKCanvas
+    draw_figure(window, figure, canvas)
 
 
-def make_window(ui):
-    layout = [[sg.Canvas(key='test.canvas')]]
-    window = ui.make_window('Matplotlib', layout)
-    make_figure(window)
+def make_window(ui, title='Matplotlib', canvas_key='test.canvas'):
+    layout = [[sg.Canvas(key=canvas_key)]]
+    window = ui.make_window(title, layout)
+    make_figure(window, canvas_key)
