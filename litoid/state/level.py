020e677c089e67222607b52f83f2ad7db3deb904
Tom Ritchford <tom@swirly.com>
2023-04-26 15:45:03 +0200

    Modify litoid/state/level.py, litoid/ui/controller.py

--- litoid/state/level.py
+++ litoid/state/level.py
@@ -10,3 +10,7 @@ class Level(NamedTuple):
     @property
     def canonical_value(self):
         return self.value_name or self.value
+
+    @property
+    def cv(self):
+        return self.channel, self.value
