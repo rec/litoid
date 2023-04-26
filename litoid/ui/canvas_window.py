9e8be2def982a1bcd70005d1084ba630ef01ce2b
Tom Ritchford <tom@swirly.com>
2023-04-26 14:13:19 +0200

    Reorganize canvas_window

--- litoid/ui/canvas_window.py
+++ litoid/ui/canvas_window.py
@@ -1,29 +1,32 @@
-import PySimpleGUI as sg
-import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import PySimpleGUI as sg
+import matplotlib.pyplot as plt
+import numpy as np
+
+CANVAS_KEY = 'data.canvas'
 
 
-def draw_figure(figure, canvas):
-    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-    return figure_canvas_agg
-
-
-def make_figure(window, canvas_key):
-    dataSize = 1000
-    xData = np.random.randint(100, size=dataSize)
-    yData = np.linspace(0, dataSize, num=dataSize, dtype=int)
+def add_data(window, *plottables):
+    if not plottables:
+        dataSize = 1000
+        xData = np.random.randint(100, size=dataSize)
+        yData = np.linspace(0, dataSize, num=dataSize, dtype=int)
+        plottables = xData, yData
 
     figure = plt.figure()
+    plt.plot(*plottables)
 
-    plt.plot(xData, yData, '.k')
-    canvas = window[canvas_key].TKCanvas
-    draw_figure(figure, canvas)
+    canvas = window[CANVAS_KEY].TKCanvas
+    agg = FigureCanvasTkAgg(figure, canvas)
+    agg.draw()
+    agg.get_tk_widget().pack(side='top', fill='both', expand=1)
 
 
-def make_window(ui, title='Matplotlib', canvas_key='test.canvas'):
-    layout = [[sg.Canvas(key=canvas_key)]]
-    window = ui.make_window(title, layout)
-    make_figure(window, canvas_key)
+def layout():
+    return [[sg.Canvas(key=CANVAS_KEY)]]
+
+
+def make_window(ui, title='Matplotlib', *plottables):
+    window = ui.make_window(title, layout())
+    add_data(window)
+    return window
